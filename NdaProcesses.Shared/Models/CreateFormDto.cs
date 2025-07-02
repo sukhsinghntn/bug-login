7332d8c634475d763042723d1ca3e681e457dbd4
sukhsinghntn <sukhsinghntn@gmail.com>
2025-07-02 09:23:46 -0400

    Fix search forms page

--- NdaProcesses.Shared/Models/CreateFormDto.cs
+++ NdaProcesses.Shared/Models/CreateFormDto.cs
@@ -11,7 +11,11 @@ namespace DynamicFormsApp.Server.Services
     public class CreateFormDto
     {
         public string Name { get; set; }
+        public string? Description { get; set; }
         public List<FormField> Fields { get; set; }
         public bool RequireLogin { get; set; } = true;
+        public bool NotifyOnResponse { get; set; } = false;
+        public string? NotificationEmail { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
--- NdaProcesses.Shared/Services/IEmailService.cs
+++ NdaProcesses.Shared/Services/IEmailService.cs
@@ -6,5 +6,6 @@ namespace DynamicFormsApp.Shared.Services
     public interface IEmailService
     {
         Task SendBugReportEmail(EmailModel email);
+        Task SendFormResponseNotification(string toEmail, string formName, int formId);
     }
 }
